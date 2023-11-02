9aafc79477a383949d3cd7dba59904b20f4f4b4e
Fred Emmott <git@fredemmott.com>
2023-11-01 21:48:48 -0500

    Check for extremely outdated openkneeboard versions

---
+++ src/linters/windows/OutdatedOpenKneeboardLinter.cpp
@@ -0,0 +1,60 @@
+// Copyright 2023 Fred Emmott <fred@fredemmott.com>
+// SPDX-License-Identifier: ISC
+
+#include <ShlObj.h>
+
+#include "Config_windows.hpp"
+#include "Linter.hpp"
+
+namespace FredEmmott::OpenXRLayers {
+
+// Warn about legacy versions which may indicate corrupted or outdaed
+// installations.
+//
+// These old versions used MSIX, which can lead to ACL issues. While
+// installing a new version will automatically clean these up,
+// it's then still possible to co-install an old msix afterwards.
+class OutdatedOpenKneeboardLinter final : public Linter {
+  virtual std::vector<std::shared_ptr<LintError>> Lint(
+    const std::vector<std::tuple<APILayer, APILayerDetails>>& layers) {
+    std::vector<std::shared_ptr<LintError>> errors;
+    for (const auto& [layer, details]: layers) {
+      bool outdated = false;
+      if (details.mName == "XR_APILAYER_NOVENDOR_OpenKneeboard") {
+        outdated = true;
+      } else if (details.mName != "XR_APILAYER_FREDEMMOTT_OpenKneeboard") {
+        continue;
+      }
+
+      if (Config::APILAYER_HKEY_ROOT == HKEY_CURRENT_USER) {
+        outdated = true;
+      }
+
+      if (!outdated) {
+        const std::string path = details.mLibraryPath.string();
+        if (path.find("ProgramData") != std::string::npos) {
+          outdated = true;
+        } else if (path.find("WindowsApps") != std::string::npos) {
+          outdated = true;
+        }
+      }
+
+      if (!outdated) {
+        continue;
+      }
+
+      errors.push_back(std::make_shared<LintError>(
+        std::format(
+          "{} is from an extremely outdated version of OpenKneeboard, which "
+          "may cause issues. Remove this API layer, install updates, and "
+          "remove any left over old versions from 'Add or Remove Programs'.",
+          layer.mJSONPath.string()),
+        PathSet {layer.mJSONPath}));
+    }
+    return errors;
+  }
+};
+
+static OutdatedOpenKneeboardLinter gInstance;
+
+}// namespace FredEmmott::OpenXRLayers
