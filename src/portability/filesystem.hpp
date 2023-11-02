e9257ed5b135c0696c1b2e48586b686a5f7077d1
Fred Emmott <git@fredemmott.com>
2023-11-02 16:12:45 -0500

    Simpler filesystem portability ocncept

--- src/portability/filesystem.hpp
+++ src/portability/filesystem.hpp
@@ -12,16 +12,13 @@ concept Hashable = requires(T a) {
   { std::hash<T> {}(a) } -> std::convertible_to<size_t>;
 };
 
-template <class T>
-concept NotHashable = !Hashable<T>;
 }// namespace FredEmmott::PortabilityHacks
 
 // std::hash<std::filesystem::path> is available on MSVC, but not MacOS
 template <class T>
-  requires requires(T x) {
-    { x } -> std::convertible_to<std::filesystem::path>;
-    FredEmmott::PortabilityHacks::NotHashable<T>;
-  }
+  requires(
+    std::same_as<T, std::filesystem::path>
+    && !FredEmmott::PortabilityHacks::Hashable<T>)
 struct std::hash<T> {
   std::size_t operator()(const T& p) const {
     return hash_value(p);
