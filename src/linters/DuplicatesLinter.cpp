ad259fad27a4269f42b0243621570a8f904fc273
Fred Emmott <git@fredemmott.com>
2023-12-02 07:57:23 -0600

    Detect duplicate layers
    fixes #3 - Warn if multiple copies of the same layer are currently enabled

---
+++ src/linters/DuplicatesLinter.cpp
@@ -0,0 +1,53 @@
+// Copyright 2023 Fred Emmott <fred@fredemmott.com>
+// SPDX-License-Identifier: ISC
+
+#include <fmt/core.h>
+
+#include <unordered_map>
+#include <vector>
+
+#include "Linter.hpp"
+
+namespace FredEmmott::OpenXRLayers {
+
+// Detect multiple enabled versions of the same layer
+class DuplicatesLinter final : public Linter {
+ public:
+  virtual std::vector<std::shared_ptr<LintError>> Lint(
+    const std::vector<std::tuple<APILayer, APILayerDetails>>& layers) {
+    std::unordered_map<std::string, PathSet> byName;
+    for (const auto& [layer, details]: layers) {
+      if (!layer.mIsEnabled) {
+        continue;
+      }
+
+      if (details.mState != APILayerDetails::State::Loaded) {
+        continue;
+      }
+
+      if (byName.contains(details.mName)) {
+        byName.at(details.mName).emplace(layer.mJSONPath);
+      } else {
+        byName[details.mName] = {layer.mJSONPath};
+      }
+    }
+
+    std::vector<std::shared_ptr<LintError>> errors;
+    for (const auto& [name, paths]: byName) {
+      if (paths.size() == 1) {
+        continue;
+      }
+
+      auto text = fmt::format("Multiple copies of {} are enabled:", name);
+      for (const auto& path: paths) {
+        text += fmt::format("\n- {}", path.string());
+      }
+
+      errors.push_back(std::make_shared<LintError>(text, paths));
+    }
+    return errors;
+  }
+};
+
+static DuplicatesLinter gInstance;
+}// namespace FredEmmott::OpenXRLayers
