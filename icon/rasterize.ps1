dc5d04642fab1e4927f94a2eb2b27d9951473f16
Fred Emmott <git@fredemmott.com>
2024-01-05 18:55:22 -0600

    actually better icon quality

--- icon/rasterize.ps1
+++ icon/rasterize.ps1
@@ -1,6 +1,20 @@
+$nativeSize = 32
+$sizes = 16,32,48,64,128,256
+$intermediates = @()
+foreach ($size in $sizes) {
+  $out = "icon-${size}.png"
+  magick convert `
+    -background none `
+    -density $((72 * $size) / $nativeSize) `
+    "$(Get-Location)\icon.svg" `
+    -gravity center `
+    -extent "${size}x${size}" `
+    png:$out
+  $intermediates += $out
+}
+Remove-Item icon.ico
 magick convert `
   -background none `
-  -density 256 `
-  "$(Get-Location)\icon.svg" `
-  -gravity center `
-  -define "icon:auto-resize" icon.ico
+  @intermediates `
+  icon.ico
+Remove-Item $intermediates
