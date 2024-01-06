e0eb045b71ef0f2802dd50623bbd38a6496310ff
Fred Emmott <git@fredemmott.com>
2024-01-05 18:25:28 -0600

    Remove obsolete comments

--- icon/rasterize.ps1
+++ icon/rasterize.ps1
@@ -1,9 +1,3 @@
-# If you're using this as an example for something else and having trouble:
-# imagemagick requires that '(' and ')' are standalone arguments (i.e. distinct
-# entries in argv) - "(foo)" is invalid, but "(" "foo" ")" is valid.
-#
-# Here, I use `--%` to disable powershell's usual argument parsing for the rest
-# of the command
 magick convert `
   -background none `
   -density 256 `
