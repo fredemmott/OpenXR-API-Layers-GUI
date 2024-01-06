e1bdf4c2c1ae49e83e333f5bafe66eba6b6b7989
Fred Emmott <git@fredemmott.com>
2024-01-05 18:25:12 -0600

    Improve icon quality

--- icon/rasterize.ps1
+++ icon/rasterize.ps1
@@ -6,7 +6,7 @@
 # of the command
 magick convert `
   -background none `
+  -density 256 `
   "$(Get-Location)\icon.svg" `
   -gravity center `
-  -trim `
-  --% ( -clone 0 -resize 16x16 -extent 16x16 ) ( -clone 0 -resize 32x32 -extent 32x32 ) ( -clone 0 -resize 48x48 -extent 48x48 ) ( -clone 0 -resize 64x64 -extent 64x64 ) ( -clone 0 -resize 128x128 -extent 128x128 ) ( -clone 0 -resize 256x256 -extent 256x256 ) icon.ico
+  -define "icon:auto-resize" icon.ico
