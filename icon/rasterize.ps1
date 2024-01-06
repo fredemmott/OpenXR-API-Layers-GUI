a53e298838a69891336a6968b6e7030d22577dff
Fred Emmott <git@fredemmott.com>
2024-01-05 19:21:03 -0600

    yet again, improve visual quality

--- icon/rasterize.ps1
+++ icon/rasterize.ps1
@@ -1,11 +1,12 @@
 $nativeSize = 32
+$nativeDpi = 96
 $sizes = 16,32,48,64,128,256
 $intermediates = @()
 foreach ($size in $sizes) {
   $out = "icon-${size}.png"
   magick convert `
     -background none `
-    -density $((72 * $size) / $nativeSize) `
+    -density $(($nativeDpi * $size) / $nativeSize) `
     "$(Get-Location)\icon.svg" `
     -gravity center `
     -extent "${size}x${size}" `
