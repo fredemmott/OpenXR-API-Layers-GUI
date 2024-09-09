c846b28881a807e259db7e968a6ae30b4359593a
Fred Emmott <git@fredemmott.com>
2024-09-09 12:27:53 -0500

    Update updater to +fredemmott.2
    Fixes regex issue

--- third-party/vicius.cmake
+++ third-party/vicius.cmake
@@ -4,9 +4,9 @@ set(
 )
 file(
   DOWNLOAD
-  "https://github.com/fredemmott/autoupdates/releases/download/vicius-v1.7.813%2Bfredemmott.1/Updater-Release.exe"
+  "https://github.com/fredemmott/autoupdates/releases/download/vicius-v1.7.813%2Bfredemmott.2/Updater-Release.exe"
   ${UPDATER_EXE}
-  EXPECTED_HASH SHA256=CCA4D201391411AF330645FE17F5A37EB51FF25560B2AF7442AAF80BAB9C1D0F
+  EXPECTED_HASH SHA256=386A226F0BE1754607F3CEAD4C242566B22D6532E6A9253A23DC666C6589265C
 )
 
 return(PROPAGATE UPDATER_EXE)
