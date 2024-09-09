b83c042f9f125c91125ef26ef04c50ac48755797
Fred Emmott <git@fredemmott.com>
2024-09-09 17:02:19 -0500

    Update the updater again
    This fixes the HANDLE ARGV sometimes being treated as hexadecimal, sometimes decimal

--- third-party/vicius.cmake
+++ third-party/vicius.cmake
@@ -4,9 +4,9 @@ set(
 )
 file(
   DOWNLOAD
-  "https://github.com/fredemmott/autoupdates/releases/download/vicius-v1.7.813%2Bfredemmott.2/Updater-Release.exe"
+  "https://github.com/fredemmott/autoupdates/releases/download/vicius-v1.7.813%2Bfredemmott.3/Updater-Release.exe"
   ${UPDATER_EXE}
-  EXPECTED_HASH SHA256=386A226F0BE1754607F3CEAD4C242566B22D6532E6A9253A23DC666C6589265C
+  EXPECTED_HASH SHA256=3826B6461A9B865DB9307AECED11F33B07A21A4F9A849DBAA07C9C1DE9F012AC
 )
 
 return(PROPAGATE UPDATER_EXE)
