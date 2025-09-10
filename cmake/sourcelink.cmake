d2481c0ed88531425590fc1419236d25a97532df
Fred Emmott <git@fredemmott.com>
2025-09-10 11:29:11 -0500

    🔨 Add SOURCELINK data

---
+++ cmake/sourcelink.cmake
@@ -0,0 +1,23 @@
+set(SOURCELINK "" CACHE STRING "URL to retrieve the source")
+if (NOT SOURCELINK)
+  return()
+endif ()
+
+if (NOT CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+  message(FATAL_ERROR "SOURCELINK was specified, but not using MSVC")
+endif ()
+
+message(STATUS "Using SOURCELINK: ${SOURCELINK}")
+cmake_path(
+  NATIVE_PATH
+  CMAKE_SOURCE_DIR
+  NORMALIZE
+  NATIVE_SOURCE
+)
+string(REPLACE "\\" "\\\\" NATIVE_SOURCE "${NATIVE_SOURCE}")
+file(
+  WRITE
+  "${CMAKE_CURRENT_BINARY_DIR}/sourcelink.json"
+  "{ \"documents\": { \"${NATIVE_SOURCE}\\\\*\": \"${SOURCELINK}/*\" } }"
+)
+add_link_options("/SOURCELINK:${CMAKE_CURRENT_BINARY_DIR}/sourcelink.json")
